2627a86fcc3ed9d497af908be8deee14c2c6706b
unknown <euniceancajima@gmail.com>
2022-04-10 13:35:48 -0500

    Mejorando función exception

--- src/OptionPanel.java
+++ src/OptionPanel.java
@@ -1,31 +1,37 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class OptionPanel {
-	static String enterOne = "";
-	static String enterTwo = "";
 	static int numOne = 0;
 	static int numTwo = 0;
+	
 	public static void main(String[] args) {
 		//Realiza que ingrese 2 datos numéricos y que devuelva la diferencia
-		boolean flag = false;
 		JOptionPane.showMessageDialog(null, "Realizaremos la resta de dos años para obtener una edad\nPor favor ingresar solo números!!");
-		do {
-			enterOne = JOptionPane.showInputDialog(null, "Ingresa un año a restar");
-			enterTwo = JOptionPane.showInputDialog(null, "Ingresa un año a restar");
-			flag = notIsNumber(enterOne, enterTwo);
+		try {
+			numOne = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un año a restar"));
+			numTwo = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un año a restar"));
 			
-		} while(!flag);
+		} catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "Ingrese solo números");
+			main(args);
+			System.exit(0);
+		}
 		
 		JOptionPane.showMessageDialog(null, numOne - numTwo);
+		
+		consoleInputs();
 	}
-	public static boolean notIsNumber(String enterOne, String enterTwo) {
-		try {
-			numOne = Integer.parseInt(enterOne);
-			numTwo = Integer.parseInt(enterTwo);
-			return true;
-		} catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "Ingrese valores numéricos!");
-			return false;
+	/**
+	 * Función para obtener datos por consola
+	 *  */ 
+	public static void consoleInputs() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingrese algo por consola:");
+		String entrada = scanner.nextLine();
+		System.out.println("Esto es lo que ingresaste:"+" "+entrada);
+		
 		}
 	}
-}
